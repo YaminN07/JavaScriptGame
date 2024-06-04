261daf6b2ca5b920628b132cdcfa2351eb17ddd5
binar <yaminn@nycstudents.net>
2024-06-04 17:23:03 -0400

    added the particles

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+console.log(gsap)
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')  //context
 
@@ -69,6 +70,34 @@ class Enemy {
    }
 }
 
+class Particle {
+   constructor(x, y, radius, color, velocity){
+      this.x = x
+      this.y = y
+      this.radius = radius
+      this.color = color
+      this.velocity = velocity
+      this.alpha = 1 //tranfarency
+   }
+
+   draw() {
+       c.save()  //calling a global code
+       c.globalAlpha = this.alpha
+       c.beginPath()
+       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+       c.fillStyle = this.color
+       c.fill()
+       c.restore()
+   }
+
+   update() { // Movement of the enemy
+      this.draw()
+      this.x = this.x + this.velocity.x
+      this.y = this.y + this.velocity.y
+      this.alpha -= 0.01
+   }
+}
+
 class Lightsaber {
     constructor(x, y, w, h, color){
        this.x = x
@@ -86,10 +115,11 @@ class Lightsaber {
     }
 }
 
-const player = new Player(x, y, 30, 'blue')
+const player = new Player(x, y, 10, 'white')
 const lightsaber = new Lightsaber(x, y, 150, 1000, 'orange')
 const projectiles = []
 const enemies = []
+const particles = []
 
 console.log(Player)
 console.log(Lightsaber)
@@ -109,44 +139,89 @@ function spawnEnemies() {
          y = Math.random() < 0.5 ? 0 - radius : canvas.height + radius
       }
 
-      const color = 'green'
+
+      const color = `hsl(${Math.random() * 360}, 50%, 50%)` // random color; hsl-- Hue(color), Saturation & Lightness; computes the code in teh {} before putting in the string
 
       const angle = Math.atan2(
         canvas.height / 2 - y,   // destanation - star from
         canvas.width / 2 - x)
-      console.log(angle)
+      //console.log(angle)
       const velocity = {
             x: Math.cos(angle),
             y: Math.sin(angle)
         }
 
       enemies.push( new Enemy(x, y, radius, color, velocity))  // This addes the const ( a variable) to the list
-      console.log(enemies)
+      //console.log(enemies)
    }, 1000 )                 // time 1000 ms = 1 s
 }
 
+let animationId
 function animate() {
-   requestAnimationFrame(animate)
-   c.clearRect(0, 0, canvas.width, canvas.height)
-   //player.draw()
-   lightsaber.draw()
-   projectiles.forEach((projectile) => {
+   animationId = requestAnimationFrame(animate)  // This returns what frame you are on currently
+   c.fillStyle = 'rgba(0, 0, 0, 0.1)' // Gives the fade effect to the game; 0.1 changes the style in a way that gives the effect
+   c.fillRect(0, 0, canvas.width, canvas.height) //The background
+   player.draw()
+   //lightsaber.draw()
+
+   particles.forEach((particles, index) => {
+      if (particles.alpha <= 0){
+         particles.splice(index, 1)
+      } else {
+         particles.update()
+      }
+   })
+
+   projectiles.forEach((projectile, index) => {
       projectile.update()
+
+      // remove from edges of screen
+      if (
+         projectile.x + projectile.radius < 0 ||
+         projectile.x - projectile.radius > canvas.width ||
+         projectile.y + projectile.radius < 0 ||
+         projectile.y - projectile.radius > canvas.height
+      ){
+         setTimeout(() => { // removing something of the screen and the program
+            projectiles.splice(index, 1)
+         }, 0)
+      }
    })
 
    enemies.forEach((enemy, index) => {
       enemy.update()
 
+      const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y)
+
+      // end game
+      if(dist - enemy.radius - player.radius < 1) {
+         cancelAnimationFrame(animationId)
+      }
+
       projectiles.forEach((projectile, projectileIndex) => {
-         const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)      //Distance between two points
+         const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)      //Distance between two points; const in a different call back code
 
-         // Objects touch
+         // When projectiles touch enemy
          if(dist - enemy.radius - projectile.radius < 1) {
-            setTimeout(() => {
-               enemies.splice(index, 1) //remove one at index
-               projectiles.splice(projectileIndex, 1)
-            }, 0) //waits for the next frame to remove it or above code
+            // making the particles
+            for(let i = 0; i < 8; i++) {
+               particles.push(new Particle(projectile.x, projectile.y, Math.random() * 2, enemy.color,
+               {x: Math.random() - 0.5, y: Math.random() - 0.5})) //random from -0.5 to 0.5
+            }
 
+            if(enemy.radius - 7 > 5){ //checks for the bid enemy
+               gsap.to(enemy,{        // for smooth animation
+                  radius: enemy.radius -7
+               })
+               setTimeout(() => { // removing something of the screen and the program
+                  projectiles.splice(projectileIndex, 1)
+               }, 0) //waits for the next frame to remove it or above code
+            } else {
+               setTimeout(() => { // removing something of the screen and the program
+                  enemies.splice(index, 1) //remove one at index
+                  projectiles.splice(projectileIndex, 1)
+               }, 0) //waits for the next frame to remove it or above code
+            }
          }
       })
    })
@@ -154,15 +229,17 @@ function animate() {
 
 addEventListener('click', (event) =>  // Adds a listener to listen for mouse clicks
   {
+    console.log(projectiles)
     const angle = Math.atan2(  // This is to find the angle of the clicked to player
-    event.clientY - canvas.height / 2,
-    event.clientX - canvas.width / 2)
-    console.log(angle)
-    const velocity = {
-      x: Math.cos(angle),
-      y: Math.sin(angle)
+       event.clientY - canvas.height / 2,
+       event.clientX - canvas.width / 2
+    )
+    //console.log(angle)
+    const velocity = { // speed of the bullet
+      x: Math.cos(angle) * 5,
+      y: Math.sin(angle) * 5
     }
-    projectiles.push(new Projectile( canvas.width / 2, canvas.height / 2, 5, 'red', velocity))
+    projectiles.push(new Projectile( canvas.width / 2, canvas.height / 2, 5, 'white', velocity))
 
   })
 
